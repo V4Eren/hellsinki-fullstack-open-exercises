b66fd3ef662e89cf8f0d88d748ec3afb58c5f032
V4Eren <gumpseven64@gmail.com>
2025-11-15 15:47:08 +0300

    Unicafe part 1-6 is done

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,34 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Button element
+const Button = ({handleClick, text}) => {
+  return(
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+//Handles one unit of statistics
+const StatisticsLine = ({text, value}) => {
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+//Handles all statistics for the website
+const Statistics = ({good, neutral, bad}) => {
+  //Find averages and positive comment rates
+  const total = good + neutral + bad
+  const average = (good - bad)/total 
+  const positive = (good/total)*100.0
+
+  if (total === 0) {
+    return(
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good}/>
+        <StatisticsLine text="neutral" value={neutral}/>
+        <StatisticsLine text="bad" value={bad}/>
+        <StatisticsLine text="average" value={average}/>      
+        <StatisticsLine text="positive" value={positive + "%"}/>
+      </tbody>
+    </table>    
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodReview = () => {
+    const newGood = good + 1
+    //console.log("Good reviews " + newGood)
+    setGood(newGood)
+  }
+  
+  const handleNeutralReview = () => {
+    const newNeutral = neutral + 1
+    //console.log("Neutral reviews " + newNeutral)
+    setNeutral(newNeutral)
+  }
+
+  const handleBadReview = () => {
+    const newBad = bad + 1
+    //console.log("Bad reviews " + newBad)
+    setBad(newBad)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodReview} text={"good"}/>
+      <Button handleClick={handleNeutralReview} text={"neutral"}/>
+      <Button handleClick={handleBadReview} text={"bad"}/>
+      
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
   )
 }
 
